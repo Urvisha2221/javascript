fc8b16ae7959764e6313fa4f74b0cfa55169710e
Urvisha2221 <131829919+Urvisha2221@users.noreply.github.com>
2024-07-17 09:30:11 +0000

    hello

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=21123432
+let accountEmail="urvisha@gmail.com"
+var accountPassword="12345"
+accountCity="bhavnager"
+// accountId=12
+let accountState;
+/*
+prefar not to use var
+becuse of issue in block scope and functional scope
+ */
+accountEmail="jaydonda@gmail.com"
+accountPassword="222111"
+accountCity="surat"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,22 @@
+// "use strict"  //trate node js code in newer varsion
+// alert(3+3);  //we are useing node js not browser
+
+// console.log(3+3); //code readiblity shoud be high
+// console.log("urvisha")
+
+// let username="urvisha" //string
+// let age=21 //number
+// let isLoggedIn=false
+
+// number=> 2 or power 53
+// bigint
+// string =>""
+// boolean =>true /false1
+//null standloan value
+//undifine ex:let username;
+//symbole unique
+
+//object
+
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
