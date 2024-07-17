81f6b2a7ba660e5b027017eb37e0de0d2d12c866
Urvisha2221 <131829919+Urvisha2221@users.noreply.github.com>
2024-07-17 12:08:27 +0000

    converstion

---
+++ 01_basics/03_conversationopration.js
@@ -0,0 +1,15 @@
+let score="urvisha"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumberIn=Number(score);
+console.log(typeof  valueNumberIn);
+console.log(valueNumberIn)
+
+// "33"=>33;
+//"33abc"=>Nan;
+// true=>1;
+//false=>0;
+isLoggedIn=1;
+let booleanLoggedIn=Boolean(isLoggedIn);
+console.log()
