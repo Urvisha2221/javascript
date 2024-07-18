2f970f2684520e270e8f7365ce124b9da899beed
Urvisha2221 <131829919+Urvisha2221@users.noreply.github.com>
2024-07-18 04:30:44 +0000

    comparistion

--- 01_basics/03_conversationopration.js
+++ 01_basics/03_conversationopration.js
@@ -1,15 +1,47 @@
-let score="urvisha"
-console.log(typeof score);
-console.log(typeof(score));
+// let score="urvisha"
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueNumberIn=Number(score);
-console.log(typeof  valueNumberIn);
-console.log(valueNumberIn)
+// let valueNumberIn=Number(score);
+// console.log(typeof  valueNumberIn);
+// console.log(valueNumberIn)
 
-// "33"=>33;
-//"33abc"=>Nan;
-// true=>1;
-//false=>0;
-isLoggedIn=1;
-let booleanLoggedIn=Boolean(isLoggedIn);
-console.log()
+// // "33"=>33;
+// //"33abc"=>Nan;
+// // true=>1;
+// //false=>0;
+// isLoggedIn=1;
+// let booleanLoggedIn=Boolean(isLoggedIn);
+// console.log()
+
+
+//*********************oprations**************** */
+
+// let value=3;
+// let navvalue=-value;
+// console.log(navvalue) ;
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2)
+// console.log(2**2);
+// console.log(2%2);
+// console.log(2/3);
+
+// let str1="urvisha";
+// let str2="donda";
+// let str3=(str1+str2)
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+1+"2");
+// console.log(1+"2"+1);
+// console.log("3"+2);
+// console.log(3+4-2*2/3%3);
+// console.log(+true);
+// console.log(+false);
+// console.log(true);
+
+let num1,num2,num3
+num1=num2=num3=2+2
+let getcounter;
+console.log(getcounter);
